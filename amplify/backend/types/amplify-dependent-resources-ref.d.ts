316534558afc592b9a5c507a6bcc206833269536
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2025-01-31 14:25:14 +0530

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,16 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "rethemprojectd8da771c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "rethemHelpGroupRole": "string"
+    }
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+  import { Authenticator } from "@aws-amplify/ui-vue";
+  import "@aws-amplify/ui-vue/styles.css";
+
+  import { Amplify } from 'aws-amplify';
+  import config from './amplifyconfiguration.json';
+  Amplify.configure(config);
+</script>
+
+<template>
+  <authenticator v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+  </authenticator>
+</template>
+
+<style>
+.amplify-button--primary {
+  background: linear-gradient(
+    to right,
+    var(--amplify-colors-green-80),
+    var(--amplify-colors-orange-40)
+  );
+}
+.background-customizable {
+  box-shadow: 0px 2px 20px rgba(0, 0, 0, 20%), 0 0 0 10000px #eee;
+}
+.amplify-label {
+  color: var(--amplify-colors-orange-80);
+}
+
+.amplify-tabs__item{
+  color: linear-gradient(
+    to left,
+    var(--amplify-colors-green-80),
+    var(--amplify-colors-orange-40)
+  );
+}
+
+.amplify-tabs__panel{
+background: var(--amplify-colors-red-10);
+}
+footer{
+  background: var(--amplify-colors-red-10); 
+}
+</style>
